92a96a2dab8efdf03e2282642a7312abc47fe241
oleg_kharebov <xarebov1993@yandex.ru>
2022-07-14 18:26:15 +0300

    first commit2

---
+++ TestStart.java
@@ -0,0 +1,12 @@
+public class TestStart extends Thread{
+    int i;
+
+    public TestStart(int i) {
+        this.i = i;
+    }
+
+    @Override
+    public void run() {
+        System.out.print(i);
+    }
+}
